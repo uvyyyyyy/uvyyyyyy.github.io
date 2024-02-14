dfef75048ef223b603383edbcbccff6ad9ad75e3
uvyyyyyy <155640764+uvyyyyyy@users.noreply.github.com>
2024-02-14 15:48:27 +0200

    Create sprite.js

---
+++ JS/sprite.js
@@ -0,0 +1,14 @@
+// JavaScript code to control the sprite animation
+
+// Get the sprite container element
+var spriteContainer = document.querySelector('.cat');
+
+// Add the 'sprite-animation' class to start the animation
+spriteContainer.classList.add('cat');
+
+
+// Function to move Dorothy to a new position
+function moveCat() {
+    cat.style.left = '250px';  // New left position
+    cay.style.top = '240px';   // New top position
+}
