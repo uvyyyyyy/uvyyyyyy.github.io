5e6551fdfdcddfdfc438f6d2a6c477530cc59a66
uvyyyyyy <155640764+uvyyyyyy@users.noreply.github.com>
2024-02-15 17:17:58 +0200

    Create script.js

---
+++ tirgul/script.js
@@ -0,0 +1,9 @@
+function sayHi () {
+    alert (" Hi there!");
+}
+function divClicked () {
+    alert (document.getElementById("dorothy").innerHTML);
+    document.getElementById("dorothy").style.left= '300px';
+    document.getElementById("dorothy").style.top= '400px';
+
+}
