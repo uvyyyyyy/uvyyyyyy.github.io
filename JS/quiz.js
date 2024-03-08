b8af063b38573ab356d3447c276c112d4fcec1fe
uvyyyyyy <155640764+uvyyyyyy@users.noreply.github.com>
2024-03-08 16:02:59 +0200

    commit

--- JS/quiz.js
+++ JS/quiz.js
@@ -7,6 +7,7 @@ function hideQuestions() {
 	document.getElementById("Three").style.display = 'none';
     document.getElementById("Four").style.display = 'none';
 	document.getElementById("Five").style.display = 'none';
+	document.getElementById("everyone").style.display = 'block';
 	document.getElementById("Next").onclick = showJustOne;
 }
 function showJustOne() {
@@ -15,6 +16,8 @@ function showJustOne() {
 	document.getElementById("Three").style.display = 'none';
     document.getElementById("Four").style.display = 'none';
 	document.getElementById("Five").style.display = 'none';
+	document.getElementById("everyone").style.display = 'none';
+	document.getElementById("krab").style.display = 'block';
 	document.getElementById("Next").onclick = showJustTwo;
 
 
@@ -26,6 +29,8 @@ function showJustTwo() {
 	document.getElementById("Three").style.display = 'none';
     document.getElementById("Four").style.display = 'none';
 	document.getElementById("Five").style.display = 'none';
+	document.getElementById("krab").style.display = 'none';
+	document.getElementById("spongy").style.display = 'block';
 	document.getElementById("Next").onclick = showJustThree;
 	document.getElementById("Back").onclick = showJustOne;
 
@@ -38,6 +43,8 @@ function showJustThree() {
 	document.getElementById("Three").style.display = 'block';
     document.getElementById("Four").style.display = 'none';
 	document.getElementById("Five").style.display = 'none';
+	document.getElementById("spongy").style.display = 'none';
+	document.getElementById("patrik").style.display = 'block';
 	document.getElementById("Next").onclick = showJustFour;
 	document.getElementById("Back").onclick = showJustTwo;
 
@@ -50,6 +57,8 @@ function showJustFour() {
 	document.getElementById("Three").style.display = 'none';
     document.getElementById("Four").style.display = 'block';
 	document.getElementById("Five").style.display = 'none';
+	document.getElementById("patrik").style.display = 'none';
+	document.getElementById("squidy").style.display = 'block';
 	document.getElementById("Next").onclick = showJustFive;
 	document.getElementById("Back").onclick = showJustThree;
 
@@ -62,6 +71,8 @@ function showJustFive() {
 	document.getElementById("Three").style.display = 'none';
     document.getElementById("Four").style.display = 'none';
 	document.getElementById("Five").style.display = 'block';
+	document.getElementById("squidy").style.display = 'none';
+	document.getElementById("plank").style.display = 'block';
 	document.getElementById("Back").onclick = showJustFour;
 
 }
@@ -79,23 +90,28 @@ function saveAnswerKrab() {
 }
 
 function finalScore() {
+	document.getElementById("plank").style.display = 'none';
+	document.getElementById("Five").style.display = 'none';
+
 	if(Spongbob> Sqiud && Spongbob> Krab){
-		alert ("you are spongebob!");
+		document.getElementById("spongwin").style.display = 'block';
 	}
-	if(Krab> Sqiud && Spongbob < Krab){
-		alert ("you are Krab!");
+	if(Krab> Sqiud &&  Krab > Spongbob){
+		document.getElementById("krabwin").style.display = 'block';
 	}
 	if(Sqiud> Spongbob && Sqiud > Krab){
-		alert ("you are Squid!");
+		document.getElementById("squidwin").style.display = 'block';
 	}
+	if(Sqiud == Spongbob ){
+		document.getElementById("spongesquid").style.display = 'block';
+	}
+	if(Sqiud == Krab ){
+		document.getElementById("krabsquid").style.display = 'block';
+	}
+	if(Spongbob == Krab ){
+		document.getElementById("krabsponge").style.display = 'block';
+	}
+
 }
-
-const answers = document.querySelectorAll('.answer');
-
-// Remove any existing 'chosen-answer' class from all answers
-answers.forEach(answer => answer.classList.remove('chosen-answer'));
-
-// Add 'chosen-answer' class to the chosen answer
-answers[chosenAnswerIndex].classList.add('chosen-answer');
 
 
