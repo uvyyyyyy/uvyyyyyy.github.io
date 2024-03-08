3e6514c54300b9db38a18a05d4169088fd9e8b59
uvyyyyyy <155640764+uvyyyyyy@users.noreply.github.com>
2024-03-08 16:19:49 +0200

    Update quiz.js

--- JS/quiz.js
+++ JS/quiz.js
@@ -7,6 +7,12 @@ function hideQuestions() {
 	document.getElementById("Three").style.display = 'none';
     document.getElementById("Four").style.display = 'none';
 	document.getElementById("Five").style.display = 'none';
+	document.getElementById("spongwin").style.display = 'none';
+	document.getElementById("krabwin").style.display = 'none';
+	document.getElementById("squidwin").style.display = 'none';
+	document.getElementById("spongesquid").style.display = 'none';
+	document.getElementById("krabsquid").style.display = 'none';
+	document.getElementById("krabsponge").style.display = 'none';
 	document.getElementById("everyone").style.display = 'block';
 	document.getElementById("Next").onclick = showJustOne;
 }
